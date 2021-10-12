9b0f0a217fa52dcf3eb96788dd5f96a5090ba069
Sergio Torres <sdtorresl@innovaciones.co>
2021-10-12 11:28:38 -0500

    Initial Home

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'home_controller.dart';
+import 'package:titled_navigation_bar/titled_navigation_bar.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -15,8 +16,9 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Counter'),
+        title: const Text('Tech Frenetic'),
       ),
+      endDrawer: const Drawer(),
       body: StreamBuilder(
         stream: store.counterStream,
         builder: (context, snapshot) => Text('${snapshot.data}'),
@@ -25,8 +27,27 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
         onPressed: () {
           store.increment();
         },
-        child: Icon(Icons.add),
+        child: const Icon(Icons.article_outlined),
       ),
+      bottomNavigationBar: TitledBottomNavigationBar(
+          reverse: true,
+          currentIndex: 2, // Use this to update the Bar giving a position
+          onTap: (index) {
+            print("Selected Index: $index");
+          },
+          items: [
+            TitledNavigationBarItem(
+                title: const Text('Community'),
+                icon: const Icon(Icons.people_alt_outlined)),
+            TitledNavigationBarItem(
+                title: const Text('Skills'), icon: const Icon(Icons.home)),
+            TitledNavigationBarItem(
+                title: const Text('T. Vendors'),
+                icon: const Icon(Icons.card_travel)),
+            TitledNavigationBarItem(
+                title: const Text('Profile'),
+                icon: const Icon(Icons.person_outline)),
+          ]),
     );
   }
 }
