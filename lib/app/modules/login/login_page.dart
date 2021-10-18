6373b6f5024a16b20ee1927476e4c9182f8a78a3
Sergio Torres <sdtorresl@innovaciones.co>
2021-10-17 22:33:57 -0500

    Login Page

--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -11,39 +11,43 @@ class LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-          iconTheme: const IconThemeData(color: Colors.black)),
-      body: ListView(
-        //mainAxisAlignment: MainAxisAlignment.start,
-        //crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          _maintitle(context),
-          const SizedBox(
-            height: 45,
-          ),
-          _createaccount(context),
-          const SizedBox(
-            height: 30,
-          ),
-          _credentialfields(context),
-          const SizedBox(
-            height: 30,
-          ),
-          _forgotpassword(context),
-          const SizedBox(
-            height: 30,
-          ),
-          _logingbutton(context),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: ListView(
+          //mainAxisAlignment: MainAxisAlignment.start,
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SafeArea(
+              child: SizedBox(
+                height: 50,
+              ),
+            ),
+            _mainTitle(context),
+            const SizedBox(
+              height: 45,
+            ),
+            _createAccount(context),
+            const SizedBox(
+              height: 30,
+            ),
+            _loginForm(context),
+            const SizedBox(
+              height: 30,
+            ),
+            _forgotPassword(context),
+            const SizedBox(
+              height: 30,
+            ),
+            _logingButton(context),
+          ],
+        ),
       ),
     );
   }
 
-  Widget _maintitle(context) {
+  Widget _mainTitle(context) {
     return Column(
-      children: [
+      children: const [
         Text('Login to',
             style: TextStyle(
               fontWeight: FontWeight.w900,
@@ -61,32 +65,36 @@ class LoginPageState extends State<LoginPage> {
     );
   }
 
-  Widget _createaccount(context) {
+  Widget _createAccount(context) {
     return Column(
-      children: [
-        Text('Don´t you have an account yet?',
-            style: TextStyle(
-              fontWeight: FontWeight.w400,
-              fontSize: 20,
-              fontFamily: 'NunitoSan',
-              color: Color.fromRGBO(5, 20, 47, 1),
-            )),
-        Text('Create one',
-            style: TextStyle(
-                fontWeight: FontWeight.w400,
-                fontSize: 20,
-                decoration: TextDecoration.underline,
-                fontFamily: 'NunitoSan',
-                color: Color.fromRGBO(5, 113, 232, 1))),
+      children: const [
+        Text(
+          'Don´t you have an account yet?',
+          style: TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 20,
+            fontFamily: 'NunitoSan',
+            color: Color.fromRGBO(5, 20, 47, 1),
+          ),
+        ),
+        Text(
+          'Create one',
+          style: TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 20,
+            decoration: TextDecoration.underline,
+            fontFamily: 'NunitoSan',
+            color: Color.fromRGBO(5, 113, 232, 1),
+          ),
+        ),
       ],
     );
   }
 
-  Widget _credentialfields(context) {
+  Widget _loginForm(context) {
     return Column(
-      children: [
-        Container(
-            child: TextField(
+      children: const [
+        TextField(
           keyboardType: TextInputType.text,
           decoration: InputDecoration(
             prefixIcon: Icon(
@@ -96,10 +104,9 @@ class LoginPageState extends State<LoginPage> {
             hintText: "Email",
             hintStyle: TextStyle(color: Colors.grey),
           ),
-        )),
+        ),
         SizedBox(height: 30),
-        Container(
-            child: TextField(
+        TextField(
           keyboardType: TextInputType.text,
           decoration: InputDecoration(
             prefixIcon: Icon(
@@ -109,38 +116,42 @@ class LoginPageState extends State<LoginPage> {
             hintText: "Password",
             hintStyle: TextStyle(color: Colors.grey),
           ),
-        )),
+        ),
       ],
     );
   }
 
-  Widget _forgotpassword(context) {
-    return Center(
-      child: Text('Forgot your password',
-          style: TextStyle(
-              fontWeight: FontWeight.w400,
-              fontSize: 20,
-              decoration: TextDecoration.underline,
-              fontFamily: 'NunitoSan',
-              color: Colors.black)),
+  Widget _forgotPassword(context) {
+    return const Center(
+      child: Text(
+        'Forgot your password',
+        style: TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 20,
+            decoration: TextDecoration.underline,
+            fontFamily: 'NunitoSan',
+            color: Colors.black),
+      ),
     );
   }
 
-  Widget _logingbutton(context) {
+  Widget _logingButton(context) {
     return GestureDetector(
-      child: Card(
+      child: const Card(
         color: Color.fromRGBO(0, 110, 232, 1),
         child: Padding(
           padding: EdgeInsets.symmetric(
             vertical: 10,
           ),
           child: Center(
-            child: Text('Login',
-                style: TextStyle(
-                    fontWeight: FontWeight.w700,
-                    fontSize: 20,
-                    fontFamily: 'NunitoSan',
-                    color: Colors.white)),
+            child: Text(
+              'Login',
+              style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20,
+                  fontFamily: 'NunitoSan',
+                  color: Colors.white),
+            ),
           ),
         ),
       ),
