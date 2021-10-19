48aa28d3201f6894746e07ea1ae111d769905787
Carolina Torres <nctorresl@innovaciones.co>
2021-10-19 10:48:56 -0500

    style changes in feed page

--- lib/app/modules/community/community_page.dart
+++ lib/app/modules/community/community_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:techfrenetic/app/widgets/post_widget.dart';
+import 'package:techfrenetic/app/widgets/meetups.dart';
 
 class CommunityPage extends StatefulWidget {
   final String title;
@@ -51,24 +52,97 @@ class CommunityPageState extends State<CommunityPage> {
     );
   }
 
+  Widget _postbox() {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            top: BorderSide(
+              width: 1.90,
+              color: Theme.of(context).primaryColor,
+            ),
+            left: BorderSide(
+              width: 0.50,
+              color: Colors.grey.withOpacity(.6),
+            ),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Theme.of(context).unselectedWidgetColor,
+              spreadRadius: -3,
+              blurRadius: 5,
+              offset: Offset(0.5, 0.5),
+            )
+          ],
+        ),
+        child: Card(
+          elevation: 0,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.zero,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Column(children: <Widget>[
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Share an update'),
+                  ),
+                  SizedBox(
+                    width: 110,
+                  ),
+                  Icon(Icons.videocam),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  //Icon(Icons.mode_edit_outline_sharp),
+                ],
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    //focusedBorder: InputBorder.none,
+                    //disabledBorder: InputBorder.none,
+                    hintText: "Write something...",
+                    hintStyle: Theme.of(context).inputDecorationTheme.hintStyle,
+                    prefixIcon: Icon(
+                      Icons.account_circle,
+                      color: Theme.of(context).splashColor,
+                    )),
+              ),
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+
   Widget _feeds() {
     return ListView(
-      children: const [
-        PostWidget(),
-        PostWidget(),
-        PostWidget(),
-        PostWidget(),
-        PostWidget(),
-        PostWidget(),
-        PostWidget(),
-        PostWidget(),
-        PostWidget(),
+      shrinkWrap: true,
+      children: [
+        SizedBox(
+          height: 20,
+        ),
+        _postbox(),
+        const PostWidget(),
+        const PostWidget(),
+        const PostWidget(),
+        const PostWidget(),
+        const PostWidget(),
+        const PostWidget(),
+        const PostWidget(),
+        const PostWidget(),
+        const PostWidget(),
       ],
     );
   }
 
   Widget _meetups() {
-    return const Text("Meetups");
+    return MeetupWidget();
   }
 
   Widget _groups() {
--- lib/app/widgets/post_widget.dart
+++ lib/app/widgets/post_widget.dart
@@ -14,57 +14,103 @@ class _PostWidgetState extends State<PostWidget> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10),
-      child: Card(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.zero,
-        ),
-        elevation: 2,
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  CircleAvatar(
-                    child: ClipOval(
-                      child: SvgPicture.asset(
-                        'assets/img/avatars/avatar-02.svg',
-                        semanticsLabel: 'Acme Logo',
-                      ),
-                    ),
-                    radius: 20,
-                    backgroundColor: Colors.grey[200],
-                  ),
-                  const SizedBox(width: 20),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Sergio",
-                        style: Theme.of(context)
-                            .textTheme
-                            .headline6!
-                            .copyWith(fontSize: 15),
-                      ),
-                      const Text("Profession 1 - 13 days ago")
-                    ],
-                  )
-                ],
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border(
+              bottom: BorderSide(
+                width: 1.00,
+                color: Theme.of(context).unselectedWidgetColor,
               ),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 15),
-                child: Text("Hola"),
+              left: BorderSide(
+                width: 0.50,
+                color: Colors.grey.withOpacity(.6),
               ),
-              CachedNetworkImage(
-                placeholder: (context, value) =>
-                    const LinearProgressIndicator(),
-                errorWidget: (context, value, e) => const Icon(Icons.error),
-                imageUrl:
-                    "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201810/stockvault-person-studying-and-learning---knowledge-concept178241_0-647x363.jpeg?0LocAW2E2gIBzZp0oZSWzxmQTvAPhN_v&size=1200:675",
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Theme.of(context).unselectedWidgetColor,
+                spreadRadius: -5,
+                blurRadius: 5,
+                offset: Offset(1.9, 1.7),
               )
-            ],
+            ]),
+        child: Card(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.zero,
+          ),
+          elevation: 0,
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      child: ClipOval(
+                        child: SvgPicture.asset(
+                          'assets/img/avatars/avatar-02.svg',
+                          semanticsLabel: 'Acme Logo',
+                        ),
+                      ),
+                      radius: 20,
+                      backgroundColor: Colors.grey[200],
+                    ),
+                    const SizedBox(width: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Sergio",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline6!
+                              .copyWith(fontSize: 15),
+                        ),
+                        const Text("Profession 1 - 13 days ago")
+                      ],
+                    )
+                  ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  child: Text("Hola"),
+                ),
+                CachedNetworkImage(
+                  placeholder: (context, value) =>
+                      const LinearProgressIndicator(),
+                  errorWidget: (context, value, e) => const Icon(Icons.error),
+                  imageUrl:
+                      "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201810/stockvault-person-studying-and-learning---knowledge-concept178241_0-647x363.jpeg?0LocAW2E2gIBzZp0oZSWzxmQTvAPhN_v&size=1200:675",
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'territorio',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6!
+                        .copyWith(fontSize: 15),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Text('4 viwes'),
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.lightbulb),
+                    Text('Cool'),
+                    Icon(Icons.lightbulb),
+                    Text('Comment'),
+                    Icon(Icons.lightbulb),
+                    Text('Share'),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
