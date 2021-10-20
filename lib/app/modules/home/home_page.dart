fdc447fba90e0d93c755878e19c1a32bc25c9c04
Carolina Torres <nctorresl@innovaciones.co>
2021-10-20 10:57:02 -0500

    Translations

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:techfrenetic/app/modules/community/community_page.dart';
-import 'package:techfrenetic/app/modules/profile/profile_page.dart';
-import 'package:techfrenetic/app/widgets/expandable_fab.dart';
 import 'home_controller.dart';
 import 'package:titled_navigation_bar/titled_navigation_bar.dart';
 import 'package:techfrenetic/app/widgets/drawer.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -21,7 +19,7 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
     const CommunityPage(),
     const Text("Skills"),
     const Text("Vendors"),
-    const ProfilePage()
+    const Text("Profile")
   ];
 
   int _currentIndex = 0;
@@ -34,157 +32,40 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    final _scaffoldKey = GlobalKey<ScaffoldState>();
-
     return Scaffold(
-      key: _scaffoldKey,
       appBar: AppBar(
-        title: Row(
-          children: [
-            const SizedBox(width: 20),
-            Image.asset('assets/img/main-logo.png', fit: BoxFit.fill),
-          ],
-        ),
-        backgroundColor: Colors.white,
-        iconTheme: const IconThemeData(color: Colors.black),
-        actions: [_searchMenu(), _userMenu(), _openDrawer(_scaffoldKey)],
-      ),
+          title: Expanded(
+            child: Row(
+              children: [
+                const SizedBox(width: 20),
+                Image.asset('assets/img/main-logo.png', fit: BoxFit.fill),
+              ],
+            ),
+          ),
+          backgroundColor: Colors.white,
+          iconTheme: const IconThemeData(color: Colors.black)),
       endDrawer: const CustomDrawer(),
-      body: _pages[_currentIndex],
+      body: AnimatedContainer(
+        duration: const Duration(seconds: 10),
+        child: _pages[_currentIndex],
+      ),
       floatingActionButton: _floatingActionButton(),
       bottomNavigationBar: _bottomNavigationBar(),
     );
   }
 
-  IconButton _openDrawer(GlobalKey<ScaffoldState> _scaffoldKey) {
-    return IconButton(
-      icon: const Icon(Icons.menu),
+  FloatingActionButton _floatingActionButton() {
+    return FloatingActionButton(
       onPressed: () {
-        _scaffoldKey.currentState!.openEndDrawer();
+        store.increment();
       },
-    );
-  }
-
-  PopupMenuButton<int> _searchMenu() {
-    return PopupMenuButton(
-      icon: const Icon(Icons.search),
-      color: Colors.blue,
-      itemBuilder: (context) => [
-        const PopupMenuItem<int>(
-          value: 0,
-          child: Text(
-            "Setting",
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-      ],
-      onSelected: (item) => {debugPrint(item.toString())},
-    );
-  }
-
-  PopupMenuButton<int> _userMenu() {
-    return PopupMenuButton(
-      child: Container(
-        width: 50,
-        padding: const EdgeInsets.symmetric(vertical: 8),
-        child: CircleAvatar(
-          child: ClipOval(
-            child: SvgPicture.asset(
-              'assets/img/avatars/avatar-01.svg',
-              semanticsLabel: 'Acme Logo',
-            ),
-          ),
-          radius: 20,
-          backgroundColor: Colors.grey[200],
-        ),
-      ),
-      offset: const Offset(0, 60),
-      color: Colors.white,
-      itemBuilder: (context) => [
-        const PopupMenuItem<int>(
-          value: 0,
-          child: Text(
-            "Notifications",
-            style: TextStyle(color: Colors.black),
-          ),
-        ),
-        const PopupMenuItem<int>(
-          value: 1,
-          child: Text(
-            "Profile",
-            style: TextStyle(color: Colors.black),
-          ),
-        ),
-        const PopupMenuItem<int>(
-          value: 2,
-          child: Text(
-            "Logout",
-            style: TextStyle(color: Colors.black),
-          ),
-        ),
-      ],
-      onSelected: (item) => {
-        debugPrint(
-          item.toString(),
-        ),
-      },
-    );
-  }
-
-  Widget _floatingActionButton() {
-    List<Widget> actions = [
-      Column(
-        children: [
-          ActionButton(
-            onPressed: () {
-              debugPrint("Action pressed");
-            },
-            icon: const Icon(Icons.videocam),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Text(
-            "Share a video",
-            style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                  fontSize: 15,
-                  color: Colors.white,
-                ),
-          ),
-        ],
-      ),
-      Column(
-        children: [
-          ActionButton(
-            onPressed: () {
-              debugPrint("Action pressed");
-            },
-            icon: const Icon(Icons.document_scanner),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Text(
-            "Share an article",
-            style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                  fontSize: 15,
-                  color: Colors.white,
-                ),
-          ),
-        ],
-      ),
-    ];
-
-    return ExpandableFab(
-      distance: 95.0,
-      children: actions,
+      child: const Icon(Icons.article_outlined),
     );
   }
 
   TitledBottomNavigationBar _bottomNavigationBar() {
     return TitledBottomNavigationBar(
       reverse: true,
-      curve: Curves.decelerate,
       currentIndex:
           _currentIndex, // Use this to update the Bar giving a position
       onTap: (index) {
@@ -195,7 +76,7 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
       },
       items: [
         TitledNavigationBarItem(
-            title: const Text('Community'),
+            title: Text(AppLocalizations.of(context)!.share),
             icon: const Icon(Icons.people_alt_outlined)),
         TitledNavigationBarItem(
             title: const Text('Skills'), icon: const Icon(Icons.home)),
