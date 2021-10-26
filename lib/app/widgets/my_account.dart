626a23dfbc1b056a15b880fc9934cba2e24aaccb
Carolina Torres <nctorresl@innovaciones.co>
2021-10-25 19:03:16 -0500

    Corrections of my account page style

--- lib/app/widgets/my_account.dart
+++ lib/app/widgets/my_account.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class MyAccountPage extends StatefulWidget {
@@ -9,6 +10,7 @@ class MyAccountPage extends StatefulWidget {
 }
 
 class _MyAccountPageState extends State<MyAccountPage> {
+  DateTime selectedDate = DateTime.now();
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -37,53 +39,105 @@ class _MyAccountPageState extends State<MyAccountPage> {
         ),
         child: ListView(
           children: [
-            Card(
-              elevation: 0,
-              child: Text(
-                AppLocalizations.of(context)!.my_account,
-                style: Theme.of(context).textTheme.headline1!.copyWith(
-                      color: Theme.of(context).indicatorColor,
-                      backgroundColor: Theme.of(context).backgroundColor,
-                    ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                elevation: 0,
+                child: Text(
+                  AppLocalizations.of(context)!.my_account,
+                  style: Theme.of(context).textTheme.headline1!.copyWith(
+                        color: Theme.of(context).indicatorColor,
+                        backgroundColor: Theme.of(context).backgroundColor,
+                      ),
+                ),
               ),
-            ),
-            Text(
-              'General Account Settings',
-              style: Theme.of(context).textTheme.headline1,
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border(
-                    top: BorderSide(
-                      width: 1.90,
-                      color: Theme.of(context).unselectedWidgetColor,
-                    ),
+              child: Text(
+                'General Account Settings',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+            ),
+            _form(context),
+            _passwordchange(context),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 50.0),
+              child: Column(
+                children: const [
+                  Text('Subscribtion'),
+                  Text('You don´t have a subscribtion yet')
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _form(context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            top: BorderSide(
+              width: .9,
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15.0),
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  Row(
+                    children: const [
+                      Text('Email'),
+                      SizedBox(
+                        width: 130,
+                      ),
+                      Text('Your country'),
+                    ],
                   ),
-                ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: "your email",
+                            hintStyle: Theme.of(context)
+                                .textTheme
+                                .bodyText1!
+                                .copyWith(
+                                    color: Theme.of(context).highlightColor),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: "Your country",
+                            hintStyle: Theme.of(context)
+                                .textTheme
+                                .bodyText1!
+                                .copyWith(
+                                    color: Theme.of(context).highlightColor),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
                 child: Column(
                   children: [
-                    Row(
-                      children: const [
-                        Text('Email'),
-                        SizedBox(
-                          width: 130,
-                        ),
-                        Text('Your country'),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextFormField(),
-                        ),
-                        Expanded(
-                          child: TextFormField(),
-                        ),
-                      ],
-                    ),
                     Row(
                       children: const [
                         Text('Mobile phone'),
@@ -96,68 +150,106 @@ class _MyAccountPageState extends State<MyAccountPage> {
                     Row(
                       children: [
                         Expanded(
-                          child: TextFormField(),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              hintText: "phone number",
+                              hintStyle: Theme.of(context)
+                                  .textTheme
+                                  .bodyText1!
+                                  .copyWith(
+                                      color: Theme.of(context).highlightColor),
+                            ),
+                          ),
                         ),
                         Expanded(
-                          child: TextFormField(),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              hintText: "dd/mm/yyyy",
+                              hintStyle: Theme.of(context)
+                                  .textTheme
+                                  .bodyText1!
+                                  .copyWith(
+                                      color: Theme.of(context).highlightColor),
+                            ),
+                            onTap: () {/*_selectDate(context);*/},
+                          ),
                         ),
                       ],
                     ),
-                    Row(
-                      children: [
-                        ElevatedButton(
-                          onPressed: () => debugPrint("Pressed"),
-                          child: Text('Save Changes'),
-                        ),
-                        ElevatedButton(
-                          onPressed: () => debugPrint("Pressed"),
-                          child: Text('Cancel'),
-                        ),
-                      ],
-                    )
                   ],
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border(
-                    bottom: BorderSide(
-                      width: 1.90,
-                      color: Theme.of(context).unselectedWidgetColor,
-                    ),
-                    top: BorderSide(
-                      width: 1.90,
-                      color: Theme.of(context).unselectedWidgetColor,
+              Row(
+                children: [
+                  const SizedBox(width: 106.5),
+                  ElevatedButton(
+                    onPressed: () => debugPrint("Pressed"),
+                    child: const Text('Save Changes'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: ElevatedButton(
+                      onPressed: () => debugPrint("Pressed"),
+                      child: const Text('Cancel'),
                     ),
                   ),
-                ),
-                child: Column(
-                  children: [
-                    Text('Password'),
-                    Row(
-                      children: [
-                        Text('Last Changed: '),
-                        ElevatedButton(
-                          onPressed: () => debugPrint("Pressed"),
-                          child: Text('Change Password'),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
+                ],
               ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // _selectDate(BuildContext context) async{
+  //    DateTime selected = await showDatePicker(
+  //     context: context,
+  //     initialDate: selectedDate,
+  //     firstDate: DateTime(1900),
+  //     lastDate: DateTime(2030),
+  //     );
+  //   if (selected != null && selected!= selectedDate )
+  //   setState(() {
+  //     selectedDate = selected;
+  //   });
+  // }
+  Widget _passwordchange(context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(
+              width: .9,
+              color: Theme.of(context).colorScheme.onSurface,
             ),
-            Column(
-              children: [
-                Text('Subscribtion'),
-                Text('You don´t have a subscribtion yet')
-              ],
+            top: BorderSide(
+              width: .9,
+              color: Theme.of(context).colorScheme.onSurface,
             ),
-          ],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15.0),
+          child: Column(
+            children: [
+              const Text('Password'),
+              Row(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text('Last Changed: ' ''),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => debugPrint("Pressed"),
+                    child: const Text('Change Password'),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
