46e9d527ef45ac7948d6485d9a7f618347c3cc1a
Carolina Torres <nctorresl@innovaciones.co>
2021-10-27 16:44:13 -0500

    Global configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-
+import 'package:global_configuration/global_configuration.dart';
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
-void main() => runApp(
-      ModularApp(
-        module: AppModule(),
-        child: const AppWidget(),
-      ),
-    );
+void main() async {
+  await GlobalConfiguration().loadFromAsset("app_settings");
+  runApp(
+    ModularApp(
+      module: AppModule(),
+      child: const AppWidget(),
+    ),
+  );
+}
