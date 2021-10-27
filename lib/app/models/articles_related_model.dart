2929911100bafaa6429a0d7b80a2d8cac4a71b00
Carolina Torres <nctorresl@innovaciones.co>
2021-10-27 16:44:42 -0500

    Articles releated model

---
+++ lib/app/models/articles_related_model.dart
@@ -0,0 +1,76 @@
+import 'dart:convert';
+
+class ArticlesReleted {
+  ArticlesReleted({
+    this.id,
+    this.title,
+    this.summary,
+    this.category,
+    this.user,
+    this.date,
+    this.slug,
+    this.displayName,
+    this.isPremium,
+    this.comments,
+    this.isVideo,
+    this.duration,
+    this.thumbnail,
+    this.image,
+  });
+
+  final String? id;
+  final String? title;
+  final String? summary;
+  final String? category;
+  final String? user;
+  final String? date;
+  final String? slug;
+  final String? displayName;
+  final bool? isPremium;
+  final String? comments;
+  final bool? isVideo;
+  final String? duration;
+  final String? thumbnail;
+  final String? image;
+
+  factory ArticlesReleted.fromJson(String str) =>
+      ArticlesReleted.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory ArticlesReleted.fromMap(Map<String, dynamic> json) => ArticlesReleted(
+        id: json["id"],
+        title: json["title"],
+        summary: json["summary"],
+        category: json["category"],
+        user: json["user"],
+        date: json["date"],
+        slug: json["slug"],
+        displayName: json["display_name"],
+        isPremium: json["is_premium"],
+        comments: json["comments"],
+        isVideo: json["is_video"] == "1",
+        duration: json["duration"],
+        thumbnail: json["thumbnail"],
+        image: json["image"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "title": title,
+        "summary": summary,
+        "category": category,
+        "date": date,
+        "slug": slug,
+        "display_name": displayName,
+        "is_premium": isPremium,
+        "comments": comments,
+        "is_video": isVideo,
+        "duration": duration,
+        "thumbnail": thumbnail,
+        "imag?e": image,
+      };
+
+  @override
+  String toString() => this.toJson();
+}
