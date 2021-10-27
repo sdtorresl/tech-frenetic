8ce0ba1f82f858f95b12fb22cc8c877314ec44e1
Carolina Torres <nctorresl@innovaciones.co>
2021-10-27 16:45:00 -0500

    Articles provider

---
+++ lib/app/providers/articles_provider.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:global_configuration/global_configuration.dart';
+import 'package:techfrenetic/app/models/articles_related_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as json;
+
+class ArticlesProvider {
+  final String _baseUrl = GlobalConfiguration().getValue("api_url");
+
+  Future<List<ArticlesReleted>> getRelatedArticles() async {
+    List<ArticlesReleted> relatedArticles = [];
+
+    try {
+      Uri _url =
+          Uri.parse(_baseUrl + "/en/v1/articles-related/articles-related");
+      var response = await http.get(_url);
+
+      if (response.statusCode == 200) {
+        debugPrint(response.body);
+
+        List<dynamic> jsonResponse = json.jsonDecode(response.body);
+
+        for (var item in jsonResponse) {
+          ArticlesReleted room = ArticlesReleted.fromMap(item);
+          relatedArticles.add(room);
+        }
+      } else {
+        debugPrint('Request failed with status: ${response.statusCode}.');
+      }
+    } on Exception {
+      debugPrint("Unexpected error");
+    }
+
+    return relatedArticles;
+  }
+}
