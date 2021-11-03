ef8f9c9437bc5282d96d82e424310c2c24f2990f
Carolina Torres <nctorresl@innovaciones.co>
2021-11-03 16:32:50 -0500

    Changing import model

--- lib/app/providers/user_provider.dart
+++ lib/app/providers/user_provider.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:global_configuration/global_configuration.dart';
 import 'package:http/http.dart' as http;
+import 'package:techfrenetic/app/models/session_model.dart';
 import 'package:techfrenetic/app/models/user_model.dart';
 import 'package:techfrenetic/app/preferences/user_preferences.dart';
 
@@ -26,6 +27,9 @@ class UserProvider {
       if (response.statusCode == 200) {
         loggedUser = SessionModel.fromJson(response.body);
         debugPrint(loggedUser.toString());
+        _prefs.csrfToken = loggedUser.csrfToken;
+        _prefs.logoutToken = loggedUser.logoutToken;
+        _prefs.currentUser = loggedUser.currentUser!.uid;
       } else {
         debugPrint('Request failed with status: ${response.statusCode}.');
       }
@@ -35,27 +39,28 @@ class UserProvider {
     return loggedUser;
   }
 
-  // Future<List<UserModel>> login(List<UserModel> userinfo,) async {
-  //   final String _baseUrl = GlobalConfiguration().getValue("api_url");
-  //   List<UserModel> userinfo = [];
+  Future <UserModel> getUserData() async {
+    final String _baseUrl = GlobalConfiguration().getValue("api_url");
+    List<UserModel> userinfo = [];
 
-  //   try {
-  //     Uri _url = Uri.parse(_baseUrl + "/api/en/user/" + userinfo + "?_format=json");
 
-  //     var response = await http.get(_url);
+    try {
+      Uri _url = Uri.parse(_baseUrl + "/api/en/user/" +  + "?_format=json");
 
-  //     if (response.statusCode == 200) {
-  //       List<dynamic> jsonResponse = json.decode(response.body)["current_user"];
-  //       for (var item in jsonResponse) {
-  //         UserModel userid = UserModel.fromMap(item);
-  //         userinfo.add(userid);
-  //       }
-  //     } else {
-  //       print('Request failed with status: ${response.statusCode}.');
-  //     }
-  //   } catch (e) {
-  //     print(e.toString());
-  //   }
-  //   return userinfo;
-  // }
+      var response = await http.get(_url);
+
+      if (response.statusCode == 200) {
+        List<dynamic> jsonResponse = json.decode(response.body)["current_user"];
+        for (var item in jsonResponse) {
+          UserModel userid = UserModel.fromMap(item);
+          userinfo.add(userid);
+        }
+      } else {
+        print('Request failed with status: ${response.statusCode}.');
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+    return userinfo;
+  }
 }
