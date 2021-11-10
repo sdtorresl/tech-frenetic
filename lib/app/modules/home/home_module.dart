a2e05b8adaf2c051e4689a82b97f6f3278fbbb88
Carolina Torres <nctorresl@innovaciones.co>
2021-11-10 12:52:43 -0500

    Sign up route

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -4,6 +4,7 @@ import 'package:techfrenetic/app/modules/articles/articles_module.dart';
 import 'package:techfrenetic/app/modules/community/community_module.dart';
 import 'package:techfrenetic/app/modules/login/login_module.dart';
 import 'package:techfrenetic/app/modules/profile/profile_page.dart';
+import 'package:techfrenetic/app/modules/sign_up/sign_up_page.dart';
 
 import 'home_controller.dart';
 import 'home_page.dart';
@@ -32,6 +33,10 @@ class HomeModule extends Module {
         ChildRoute(
           '/profile',
           child: (context, args) => const ProfilePage(),
+        ),
+        ChildRoute(
+          '/sign',
+          child: (context, args) => const SignUpPage(),
         ),
       ],
     ),
