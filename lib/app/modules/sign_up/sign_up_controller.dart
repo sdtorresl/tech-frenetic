b0b38b614db917d3c444b355b9bed24aeef30a8e
Carolina Torres <nctorresl@innovaciones.co>
2021-11-12 17:38:54 -0500

    Creating sign up controller

---
+++ lib/app/modules/sign_up/sign_up_controller.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:techfrenetic/app/models/session_model.dart';
+import 'package:techfrenetic/app/providers/user_provider.dart';
+import '../../common/validators.dart';
+
+class SignUpController extends Disposable {
+  final _emailController = BehaviorSubject<String>();
+  final _passwordController = BehaviorSubject<String>();
+
+  Stream<String> get emailStream =>
+      _emailController.stream.transform(Validators.validateEmail);
+  Stream<String> get passwordStream =>
+      _passwordController.stream.transform(Validators.validatePassword);
+
+  Stream<bool> get formValidStream =>
+      Rx.combineLatest2(emailStream, passwordStream, (e, p) => true);
+
+  Function(String) get changeEmail => _emailController.sink.add;
+  Function(String) get changePassword => _passwordController.sink.add;
+
+  String get email => _emailController.value;
+  String get password => _passwordController.value;
+
+  Future<bool?> sing() async {
+    debugPrint('Pressed');
+  }
+
+  @override
+  void dispose() {
+    _emailController.close();
+    _passwordController.close();
+  }
+}
