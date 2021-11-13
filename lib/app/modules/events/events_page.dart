571f900b3873927e682aa63978f20f5eb54e4442
Carolina Torres <nctorresl@innovaciones.co>
2021-11-12 19:30:39 -0500

    Improving events page

--- lib/app/modules/events/events_page.dart
+++ lib/app/modules/events/events_page.dart
@@ -13,170 +13,182 @@ class _EventsPageState extends State<EventsPage> {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border(
-              bottom: BorderSide(
-                width: 1.00,
-                color: Colors.grey.withOpacity(.3),
-              ),
-            ),
-          ),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Row(
-                  children: [
-                    HighlightContainer(
-                      child: Text(
-                        'Featured',
-                        style: Theme.of(context).textTheme.headline1!.copyWith(
-                            color: Theme.of(context).primaryColor,
-                            fontSize: 25),
-                      ),
-                    ),
-                    Text(
-                      ' events',
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline1!
-                          .copyWith(fontSize: 25),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 50),
-            ],
+        featureEvents(),
+        eventSerch(),
+        upcomingEvents(),
+        recentEvents(),
+      ],
+    );
+  }
+
+  Widget featureEvents() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(
+            width: 1.00,
+            color: Colors.grey.withOpacity(.3),
           ),
         ),
-        Container(
-          decoration: BoxDecoration(
-            color: Theme.of(context).splashColor.withOpacity(0.1),
-            border: Border(
-              bottom: BorderSide(
-                width: 1.00,
-                color: Colors.grey.withOpacity(.3),
-              ),
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(40.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Row(
               children: [
-                Text(
-                  'Event Serch:',
-                  style: Theme.of(context).textTheme.headline1!.copyWith(
-                      fontSize: 20, color: Theme.of(context).primaryColor),
-                ),
-                const SizedBox(height: 40),
-                Container(
-                  color: Colors.white,
-                  child: TextFormField(),
-                ),
-                const SizedBox(height: 25),
-                Center(
-                  child: SizedBox(
-                    width: 400,
-                    child: ElevatedButton(
-                      child: const Text('search'),
-                      style: ButtonStyle(
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                          const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.zero,
-                          ),
-                        ),
-                      ),
-                      onPressed: () {
-                        return debugPrint('Im working');
-                      },
-                    ),
+                HighlightContainer(
+                  child: Text(
+                    'Featured',
+                    style: Theme.of(context).textTheme.headline1!.copyWith(
+                        color: Theme.of(context).primaryColor, fontSize: 25),
                   ),
+                ),
+                Text(
+                  ' events',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline1!
+                      .copyWith(fontSize: 25),
                 ),
               ],
             ),
           ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border(
-              bottom: BorderSide(
-                width: 1.00,
-                color: Colors.grey.withOpacity(.3),
-              ),
-            ),
+          const SizedBox(height: 50),
+        ],
+      ),
+    );
+  }
+
+  Widget eventSerch() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).splashColor.withOpacity(0.1),
+        border: Border(
+          bottom: BorderSide(
+            width: 1.00,
+            color: Colors.grey.withOpacity(.3),
           ),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Row(
-                  children: [
-                    HighlightContainer(
-                      child: Text(
-                        'Upcoming',
-                        style: Theme.of(context).textTheme.headline1!.copyWith(
-                            color: Theme.of(context).primaryColor,
-                            fontSize: 25),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Event Serch:',
+              style: Theme.of(context).textTheme.headline1!.copyWith(
+                  fontSize: 20, color: Theme.of(context).primaryColor),
+            ),
+            const SizedBox(height: 40),
+            Container(
+              color: Colors.white,
+              child: TextFormField(),
+            ),
+            const SizedBox(height: 25),
+            Center(
+              child: SizedBox(
+                width: 400,
+                child: ElevatedButton(
+                  child: const Text('search'),
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
                       ),
                     ),
-                    Text(
-                      ' events',
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline1!
-                          .copyWith(fontSize: 25),
-                    ),
-                  ],
+                  ),
+                  onPressed: () {
+                    return debugPrint('Im working');
+                  },
                 ),
-              ),
-              const SizedBox(height: 50),
-            ],
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: Theme.of(context).splashColor.withOpacity(0.1),
-            border: Border(
-              bottom: BorderSide(
-                width: 1.00,
-                color: Colors.grey.withOpacity(.3),
               ),
             ),
-          ),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Row(
-                  children: [
-                    HighlightContainer(
-                      child: Text(
-                        'Recent',
-                        style: Theme.of(context).textTheme.headline1!.copyWith(
-                            color: Theme.of(context).primaryColor,
-                            fontSize: 25),
-                      ),
-                    ),
-                    Text(
-                      ' events',
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline1!
-                          .copyWith(fontSize: 25),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 70),
-            ],
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget upcomingEvents() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(
+            width: 1.00,
+            color: Colors.grey.withOpacity(.3),
           ),
         ),
-      ],
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Row(
+              children: [
+                HighlightContainer(
+                  child: Text(
+                    'Upcoming',
+                    style: Theme.of(context).textTheme.headline1!.copyWith(
+                        color: Theme.of(context).primaryColor, fontSize: 25),
+                  ),
+                ),
+                Text(
+                  ' events',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline1!
+                      .copyWith(fontSize: 25),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 50),
+        ],
+      ),
+    );
+  }
+
+  Widget recentEvents() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).splashColor.withOpacity(0.1),
+        border: Border(
+          bottom: BorderSide(
+            width: 1.00,
+            color: Colors.grey.withOpacity(.3),
+          ),
+        ),
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Row(
+              children: [
+                HighlightContainer(
+                  child: Text(
+                    'Recent',
+                    style: Theme.of(context).textTheme.headline1!.copyWith(
+                        color: Theme.of(context).primaryColor, fontSize: 25),
+                  ),
+                ),
+                Text(
+                  ' events',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline1!
+                      .copyWith(fontSize: 25),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 70),
+        ],
+      ),
     );
   }
 }
