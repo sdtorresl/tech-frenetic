6a80045c26a29919235e20a2065f501930237e8d
Carolina Torres <nctorresl@innovaciones.co>
2021-11-17 18:41:47 -0500

    Name and terms controler

--- lib/app/modules/sign_up/sign_up_controller.dart
+++ lib/app/modules/sign_up/sign_up_controller.dart
@@ -6,17 +6,34 @@ import '../../common/validators.dart';
 class SignUpController extends Disposable {
   final _emailController = BehaviorSubject<String>();
   final _passwordController = BehaviorSubject<String>();
+  final _nameController = BehaviorSubject<String>();
+  final _termsController = BehaviorSubject<bool>();
+
+  //final _confirmPasswordController = BehaviorSubject<List<String>>();
 
   Stream<String> get emailStream =>
       _emailController.stream.transform(Validators.validateEmail);
+
   Stream<String> get passwordStream =>
       _passwordController.stream.transform(Validators.validatePassword);
+
+  Stream<String> get nameStream =>
+      _nameController.stream.transform(Validators.validateName);
+
+  Stream<bool> get termsStream =>
+      _termsController.stream.transform(Validators.validateTerms);
+
+  // Stream<List<String>> get confirmPasswordStream =>
+  //     _confirmPasswordController.stream.transform(Validators.validateConfirmPasword);
 
   Stream<bool> get formValidStream =>
       Rx.combineLatest2(emailStream, passwordStream, (e, p) => true);
 
   Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changePassword => _passwordController.sink.add;
+  Function(String) get changeName => _nameController.sink.add;
+  Function(bool) get changeTerms => _termsController.sink.add;
+  //Function(String) get changeConfirmPassword => _confirmPasswordController.sink.add;
 
   String get email => _emailController.value;
   String get password => _passwordController.value;
