e2b02677cce31d2c371f1940b621d3793c9dd51c
Carolina Torres <nctorresl@innovaciones.co>
2021-11-19 21:17:10 -0500

    Route to forgot password

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -2,14 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:techfrenetic/app/modules/articles/articles_module.dart';
 import 'package:techfrenetic/app/modules/community/community_module.dart';
+import 'package:techfrenetic/app/modules/forgot_password/forgot_password_module.dart';
+
 import 'package:techfrenetic/app/modules/login/login_module.dart';
 import 'package:techfrenetic/app/modules/profile/profile_page.dart';
-
 import 'package:techfrenetic/app/modules/sign_up/sign_up_module.dart';
-import 'package:techfrenetic/app/modules/sign_up/sign_up_page.dart';
-
 import 'package:techfrenetic/app/modules/events/events_page.dart';
-
 
 import 'home_controller.dart';
 import 'home_page.dart';
@@ -44,5 +42,6 @@ class HomeModule extends Module {
     ModuleRoute('/community/article', module: ArticlesModule()),
     ModuleRoute('/login', module: LoginModule()),
     ModuleRoute('/sign', module: SignUpModule()),
+    ModuleRoute('/forgot', module: ForgotPasswordModule()),
   ];
 }
