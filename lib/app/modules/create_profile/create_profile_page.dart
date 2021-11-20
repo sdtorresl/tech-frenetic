e705caa540dca69fb7bb715e6901b2c79a3d1e19
Carolina Torres <nctorresl@innovaciones.co>
2021-11-19 22:07:46 -0500

    Create profile page

---
+++ lib/app/modules/create_profile/create_profile_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CreateProfilePage extends StatefulWidget {
+  const CreateProfilePage({Key? key}) : super(key: key);
+
+  @override
+  _CreateProfilePageState createState() => _CreateProfilePageState();
+}
+
+class _CreateProfilePageState extends State<CreateProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Create Profile'),
+    );
+  }
+}
