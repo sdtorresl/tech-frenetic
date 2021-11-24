1b86ed0588f70cfacbbcbc33e9a366b730024b1a
Carolina Torres <nctorresl@innovaciones.co>
2021-11-24 18:49:37 -0500

    Improving create profile controller

--- lib/app/modules/create_profile/create_profile_controller.dart
+++ lib/app/modules/create_profile/create_profile_controller.dart
@@ -4,61 +4,32 @@ import 'package:rxdart/rxdart.dart';
 import '../../common/validators.dart';
 
 class CreateProfileController extends Disposable {
-  final _emailController = BehaviorSubject<String>();
-  final _passwordController = BehaviorSubject<String>();
   final _nameController = BehaviorSubject<String>();
-  final _termsController = BehaviorSubject<bool>();
-  //final _passwordCheckController = BehaviorSubject<String>();
-
-  Stream<String> get emailStream =>
-      _emailController.stream.transform(Validators.validateEmail);
-
-  Stream<String> get passwordStream =>
-      _passwordController.stream.transform(Validators.validatePassword);
-  //   .doOnData(
-  // (String password) {
-  //   if (0 != _passwordCheckController.value.compareTo(password)) {
-  //     _passwordController.addError("Las contraseñas no coinciden");
-  //     debugPrint(_passwordCheckController.value);
-  //   } else {
-  //     // _passwordCheckController.sink.add(_passwordCheckController.value);
-  //     // print('Loop');
-  //   }
-  // },
-  //);
+  final _descriptionController = BehaviorSubject<String>();
 
   Stream<String> get nameStream =>
       _nameController.stream.transform(Validators.validateName);
 
-  Stream<bool> get termsStream =>
-      _termsController.stream.transform(Validators.validateTerms);
+  Stream<String> get descriptionStream =>
+      _descriptionController.stream.transform(Validators.validateName);
 
-  // Stream<List<String>> get confirmPasswordStream =>
-  //     _confirmPasswordController.stream.transform(Validators.validateConfirmPasword);
+  Stream<bool> get formValidStream =>
+      Rx.combineLatest2(nameStream, descriptionStream, (e, p) => true);
 
-  Stream<bool> get formValidStream => Rx.combineLatest4(termsStream, nameStream,
-      emailStream, passwordStream, (e, p, c, d) => true);
-
-  Function(String) get changeEmail => _emailController.sink.add;
-  Function(String) get changePassword => _passwordController.sink.add;
   Function(String) get changeName => _nameController.sink.add;
-  Function(bool) get changeTerms => _termsController.sink.add;
-  //Function(String) get changeConfirmPassword => _confirmPasswordController.sink.add;
+  Function(String) get changeDescription => _descriptionController.sink.add;
 
-  String get email => _emailController.value;
-  String get password => _passwordController.value;
   String get name => _nameController.value;
-  bool get terms => _termsController.value;
+  String get description => _descriptionController.value;
 
-  Future<bool> sing() async {
+  Future<bool> createProfile() async {
     debugPrint('Sing in');
     return true;
   }
 
   @override
   void dispose() {
-    _emailController.close();
-    _passwordController.close();
+    _descriptionController.close();
     _nameController.close();
   }
 }
