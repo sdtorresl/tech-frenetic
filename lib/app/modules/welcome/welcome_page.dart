29982bd792f6979b19dce85f94f55ebc66cd3a42
Carolina Torres <nctorresl@innovaciones.co>
2021-11-24 18:51:59 -0500

    Creating welcome page

---
+++ lib/app/modules/welcome/welcome_page.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:techfrenetic/app/widgets/highlight_container.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                HighlightContainer(
+                  child: Text(
+                    'Awesome !',
+                    style: Theme.of(context).textTheme.headline1!.copyWith(
+                        color: Theme.of(context).indicatorColor, fontSize: 25),
+                  ),
+                ),
+                Text(
+                  'Welcome to TechFrenetic',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline1!
+                      .copyWith(fontSize: 25),
+                ),
+                const SizedBox(height: 25),
+                Container(
+                  height: 2.5,
+                  width: 100,
+                  color: Colors.black,
+                ),
+                const SizedBox(height: 25),
+                Text(
+                  'Here you can',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline1!
+                      .copyWith(fontSize: 17),
+                ),
+                const SizedBox(height: 40),
+                Row(
+                  children: [
+                    Icon(Icons.article_outlined,
+                        color: Theme.of(context).primaryColor),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: Text(
+                        'Publish your professional tech profile.',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 40),
+                Row(
+                  children: [
+                    Icon(Icons.video_call_outlined,
+                        color: Theme.of(context).primaryColor),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: Text(
+                        'Highlight by creating articles and videos for the community.',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 40),
+                Row(
+                  children: [
+                    Icon(Icons.lock_open,
+                        color: Theme.of(context).primaryColor),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: Text(
+                        'Access to all public articles and videos.',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 40),
+                Row(
+                  children: [
+                    Icon(Icons.mark_chat_read_rounded,
+                        color: Theme.of(context).primaryColor),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: Text(
+                        'Interact with the tech community',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 60),
+                Center(
+                  child: SizedBox(
+                    width: 300,
+                    child: ElevatedButton(
+                      onPressed: () => Modular.to.pushNamed("/login"),
+                      child: Text(
+                        'Start now!',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline1!
+                            .copyWith(color: Colors.white),
+                      ),
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.zero),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
