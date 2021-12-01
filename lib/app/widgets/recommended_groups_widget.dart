ccf5c7c1a5785f5bcdce6d28e5ec947055e46010
Carolina Torres <nctorresl@innovaciones.co>
2021-12-01 13:41:32 -0500

    Improving recommended groups widget

--- lib/app/widgets/recommended_groups_widget.dart
+++ lib/app/widgets/recommended_groups_widget.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class RecommendedGroupsWidget extends StatefulWidget {
-  RecommendedGroupsWidget({Key? key}) : super(key: key);
+  const RecommendedGroupsWidget({Key? key}) : super(key: key);
 
   @override
   _RecommendedGroupsWidgetState createState() =>
@@ -11,28 +12,100 @@ class RecommendedGroupsWidget extends StatefulWidget {
 class _RecommendedGroupsWidgetState extends State<RecommendedGroupsWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border(
-          bottom: BorderSide(
-            width: 1.00,
-            color: Theme.of(context).primaryColor,
-          ),
-          left: BorderSide(
-            width: 0.50,
-            color: Colors.grey.withOpacity(.6),
-          ),
-          right: BorderSide(
-            width: 0.50,
-            color: Colors.grey.withOpacity(.6),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(
+              width: 1.5,
+              color: Theme.of(context).primaryColor,
+            ),
+            left: BorderSide(
+              width: 0.50,
+              color: Colors.grey.withOpacity(.6),
+            ),
+            right: BorderSide(
+              width: 0.50,
+              color: Colors.grey.withOpacity(.6),
+            ),
           ),
         ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [Text('data')],
+        child: Column(
+          children: [
+            CachedNetworkImage(
+              placeholder: (context, value) => const LinearProgressIndicator(),
+              errorWidget: (context, value, e) => const Icon(Icons.error),
+              imageUrl:
+                  "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201810/stockvault-person-studying-and-learning---knowledge-concept178241_0-647x363.jpeg?0LocAW2E2gIBzZp0oZSWzxmQTvAPhN_v&size=1200:675",
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Remote Workers',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline1!
+                        .copyWith(fontSize: 16),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '234 members',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline1!
+                                .copyWith(fontSize: 13),
+                          ),
+                          Text(
+                            '67 post',
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText1!
+                                .copyWith(fontSize: 13),
+                          ),
+                        ],
+                      ),
+                      ElevatedButton(
+                        onPressed: null,
+                        child: Text(
+                          'Join Group',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline1!
+                              .copyWith(fontSize: 13),
+                        ),
+                        style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.zero,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
