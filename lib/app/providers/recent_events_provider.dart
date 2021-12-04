f091269ce631532c32e01051f39ff6d7e3cb741d
Carolina Torres <nctorresl@innovaciones.co>
2021-12-04 09:43:22 -0500

    Creating recent events provider

---
+++ lib/app/providers/recent_events_provider.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:techfrenetic/app/models/events_model.dart';
+import 'package:techfrenetic/app/providers/tf_provider.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as json;
+
+class EventsProvider extends TechFreneticProvider {
+  Future<List<EventsModel>> getRecentEvents() async {
+    try {
+      Uri _url = Uri.parse("$baseUrl/api/$locale/v1/events-recent");
+      var response = await http.get(_url);
+
+      if (response.statusCode == 200) {
+        if (response.statusCode == 200) {
+          List<dynamic> jsonResponse = json.jsonDecode(response.body);
+          List<EventsModel> events = [];
+
+          for (var item in jsonResponse) {
+            EventsModel event = EventsModel.fromMap(item);
+            events.add(event);
+          }
+          return events;
+        } else {
+          debugPrint('Request failed with status: ${response.statusCode}.');
+        }
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+    return [];
+  }
+}
