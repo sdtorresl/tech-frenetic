9c4db269e7f2f4f8e50a2491001881f23b888339
Sergio Torres <sdtorresl@unal.edu.co>
2021-12-22 15:05:36 -0500

    Implement Auth guard

--- lib/app/core/auth_guard.dart
+++ lib/app/core/auth_guard.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:techfrenetic/app/providers/user_provider.dart';
+import 'package:techfrenetic/app/preferences/user_preferences.dart';
 
 class AuthGuard extends RouteGuard {
   AuthGuard() : super(redirectTo: '/login/');
 
   @override
   bool canActivate(String path, ModularRoute router) {
-    print("Checking.....");
-    return false;
+    UserPreferences _prefs = UserPreferences();
+    return _prefs.csrfToken?.isNotEmpty ?? false;
   }
 }
