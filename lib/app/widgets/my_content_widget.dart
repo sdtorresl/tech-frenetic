b8f5004280106dbe74af7f4f920741d977af7007
Carolina Torres <nctorresl@innovaciones.co>
2022-01-11 07:32:10 -0500

    Adding numbers of created articles

--- lib/app/widgets/my_content_widget.dart
+++ lib/app/widgets/my_content_widget.dart
@@ -58,7 +58,23 @@ class _MyContentState extends State<MyContent> {
                     ),
                   ),
                 ),
-                Text(numberOfArticles + AppLocalizations.of(context)!.articles)
+                FutureBuilder(
+                  future: _articlesProvideer.getWall(),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    int number = 0;
+                    if (snapshot.hasData) {
+                      List<ArticlesModel> contents = snapshot.data ?? [];
+                      for (ArticlesModel content in contents) {
+                        if (content.type == 'Article' &&
+                            _prefs.userName == content.user) {
+                          number++;
+                        }
+                      }
+                    }
+                    return Text(
+                        '$number ' + AppLocalizations.of(context)!.articles);
+                  },
+                ),
               ],
             ),
           ),
@@ -69,10 +85,8 @@ class _MyContentState extends State<MyContent> {
               if (snapshot.hasData) {
                 List<ArticlesModel> contents = snapshot.data ?? [];
                 List<Widget> savedPostsWidgets = [];
+
                 for (ArticlesModel content in contents) {
-                  int number = int.parse(numberOfArticles);
-                  int numberArticles = number++;
-                  numberOfArticles = numberArticles.toString();
                   if (content.type == 'Article' &&
                       _prefs.userName == content.user) {
                     debugPrint(content.toString());
