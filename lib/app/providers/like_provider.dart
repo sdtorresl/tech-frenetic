55f98c88e7f55eb73cc09dd30932dc042b372ab6
Carolina Torres <nctorresl@innovaciones.co>
2022-01-21 14:26:53 -0500

    Dislike provider and inproving like provider

--- lib/app/providers/like_provider.dart
+++ lib/app/providers/like_provider.dart
@@ -1,12 +1,18 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:techfrenetic/app/models/like_model.dart';
 import 'package:techfrenetic/app/providers/tf_provider.dart';
 
 class LikeProvider extends TechFreneticProvider {
-  Future<bool?> like(
+  String? dislikeId;
+
+  Future<LikeModel> like(
     String id,
   ) async {
+    LikeModel? likeModel;
+    LikeValueModel? value;
+
     try {
       Uri _url = Uri.parse("$baseUrl/api/entity/vote/?_format=json");
 
@@ -46,7 +52,39 @@ class LikeProvider extends TechFreneticProvider {
       debugPrint(response.body);
 
       if (response.statusCode == 201) {
-        debugPrint(response.body);
+        likeModel = LikeModel.fromJson(response.body);
+        value = LikeValueModel.fromMap(likeModel.id);
+        dislikeId = value.value.toString();
+        debugPrint(value.toString());
+        return likeModel;
+      } else {
+        debugPrint('Request failed with status: ${response.statusCode}.');
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+    return LikeModel.empty();
+  }
+
+  Future<bool> dislike() async {
+    try {
+      Uri _url = Uri.parse("$baseUrl/api/entity/vote/$dislikeId?_format=json");
+
+      Map<String, String> headers = {};
+      headers.addAll(authHeader);
+      headers.addAll(jsonHeader);
+      headers.addAll(sessionHeader);
+      headers.addAll(headers);
+
+      var response = await http.delete(
+        _url,
+        headers: headers,
+      );
+      debugPrint(response.statusCode.toString());
+      debugPrint(response.body);
+
+      if (response.statusCode == 204) {
+        debugPrint('deleated');
         return true;
       } else {
         debugPrint('Request failed with status: ${response.statusCode}.');
