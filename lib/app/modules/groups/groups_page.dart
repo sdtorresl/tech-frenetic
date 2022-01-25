c067421f213db7cfbd975d8c2691bdd32d59763a
Carolina Torres <nctorresl@innovaciones.co>
2022-01-25 11:34:29 -0500

    Fixing create tab

--- lib/app/modules/groups/groups_page.dart
+++ lib/app/modules/groups/groups_page.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:techfrenetic/app/modules/my_groups/my_groups_page.dart';
 import 'package:techfrenetic/app/modules/discover_groups/discover_groups_page.dart';
+import 'package:techfrenetic/app/widgets/highlight_container.dart';
 
 class GroupsPage extends StatefulWidget {
   const GroupsPage({Key? key}) : super(key: key);
@@ -46,11 +48,60 @@ class _GroupsPageState extends State<GroupsPage> {
             ),
           ],
         ),
-        body: const TabBarView(
+        body: TabBarView(
           children: [
-            DiscoverGroupsPage(),
-            MyGroupPage(),
-            SizedBox(),
+            const DiscoverGroupsPage(),
+            const MyGroupPage(),
+            Scaffold(
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        HighlightContainer(
+                          child: Text(
+                            AppLocalizations.of(context)!.tab_create,
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline1!
+                                .copyWith(
+                                    fontSize: 25,
+                                    color: Theme.of(context).primaryColor),
+                          ),
+                        ),
+                        Text(
+                          AppLocalizations.of(context)!.groups,
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline1!
+                              .copyWith(fontSize: 25),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 40),
+                    SizedBox(
+                      width: 100,
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: () => Modular.to
+                            .pushNamed("/community/groups/create_groups"),
+                        child: Text(
+                          AppLocalizations.of(context)!.tab_create,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText2!
+                              .copyWith(color: Colors.white),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
