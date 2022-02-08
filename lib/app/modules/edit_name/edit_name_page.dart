b09c97e7bee0083a02183ccc9077cd8e7807dfb3
Sergio Torres <sdtorresl@unal.edu.co>
2022-02-08 12:17:42 -0500

    Edit Bloc

--- lib/app/modules/edit_name/edit_name_page.dart
+++ lib/app/modules/edit_name/edit_name_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
+import 'package:techfrenetic/app/core/user_preferences.dart';
 import 'package:techfrenetic/app/models/categories_model.dart';
 import 'package:techfrenetic/app/modules/edit_name/profile_bloc.dart';
 import 'package:techfrenetic/app/providers/professions_provider.dart';
@@ -15,8 +16,20 @@ class EditNamePage extends StatefulWidget {
 ProfessionsProvider professions = ProfessionsProvider();
 
 class _EditNamePageState extends State<EditNamePage> {
-  final ProfileBloc _profileBloc = ProfileBloc();
+  late final ProfileBloc _profileBloc;
+  late UserPreferences _prefs;
   String? defaultValue;
+
+  @override
+  void initState() {
+    super.initState();
+    _profileBloc = ProfileBloc();
+    _prefs = UserPreferences();
+
+    if (_prefs.userName != null) {
+      _profileBloc.changeName(_prefs.userName!);
+    }
+  }
 
   @override
   void dispose() {
@@ -102,59 +115,67 @@ class _EditNamePageState extends State<EditNamePage> {
           const SizedBox(height: 60),
           _professionField(),
           const SizedBox(height: 60),
-          StreamBuilder(
-            stream: _profileBloc.nameStream,
-            builder: (BuildContext context, AsyncSnapshot snapshot) {
-              return ElevatedButton(
-                onPressed:
-                    snapshot.hasError || !snapshot.hasData ? null : saveName(),
-                child: Text(AppLocalizations.of(context)!.profile_save),
-              );
-            },
-          ),
+          _submitButton(),
         ],
       ),
+    );
+  }
+
+  Widget _submitButton() {
+    return StreamBuilder(
+      stream: _profileBloc.nameFormStream,
+      builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+        debugPrint("Data: ${snapshot.data.toString()}");
+        debugPrint(snapshot.hasError.toString());
+        return ElevatedButton(
+          onPressed: !snapshot.hasData ? null : saveName,
+          child: Text(AppLocalizations.of(context)!.profile_save),
+        );
+      },
     );
   }
 
   Widget _professionField() {
     return FutureBuilder(
       future: professions.getProfessions(),
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
+      builder: (BuildContext context,
+          AsyncSnapshot<List<CategoriesModel>> snapshot) {
         List<String> professionsNames = [];
         if (snapshot.hasData) {
-          List<CategoriesModel> categoriesModel = snapshot.data;
-          for (var models in categoriesModel) {
+          List<CategoriesModel> professions = snapshot.data!;
+          for (var models in professions) {
             professionsNames.add(models.category);
           }
 
-          return DropdownButton<String>(
-            value: defaultValue,
-            isExpanded: true,
-            underline: Container(
-              height: 0.5,
-              color: Colors.black,
-            ),
-            hint: Text(
-              AppLocalizations.of(context)!.profile_your_profession,
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyText1!
-                  .copyWith(color: Theme.of(context).hintColor),
-            ),
-            items: professionsNames.map(
-              (String valueItem) {
-                return DropdownMenuItem<String>(
-                  child: Text(valueItem),
-                  value: valueItem,
-                );
-              },
-            ).toList(),
-            onChanged: (newValue) {
-              setState(
-                () {
-                  defaultValue = newValue!;
-                  debugPrint(defaultValue);
+          return StreamBuilder<String>(
+            stream: _profileBloc.professionStream,
+            builder: (context, snapshot) {
+              return DropdownButton<String>(
+                value: _profileBloc.profession,
+                isExpanded: true,
+                underline: Container(
+                  height: 0.5,
+                  color: Colors.black,
+                ),
+                hint: Text(
+                  AppLocalizations.of(context)!.profile_your_profession,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText1!
+                      .copyWith(color: Theme.of(context).hintColor),
+                ),
+                items: professionsNames.map(
+                  (String valueItem) {
+                    return DropdownMenuItem<String>(
+                      child: Text(valueItem),
+                      value: valueItem,
+                    );
+                  },
+                ).toList(),
+                onChanged: (value) {
+                  if (value != null) {
+                    _profileBloc.changeProfession(value);
+                  }
                 },
               );
             },
@@ -177,6 +198,7 @@ class _EditNamePageState extends State<EditNamePage> {
       stream: _profileBloc.nameStream,
       builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
         return TextFormField(
+          initialValue: _profileBloc.name,
           onChanged: _profileBloc.changeName,
           decoration: InputDecoration(
             hintText: AppLocalizations.of(context)!.profile_your_name,
@@ -195,5 +217,8 @@ class _EditNamePageState extends State<EditNamePage> {
     );
   }
 
-  saveName() {}
+  saveName() async {
+    debugPrint(_profileBloc.name);
+    debugPrint(_profileBloc.profession);
+  }
 }
--- lib/app/modules/edit_name/profile_bloc.dart
+++ lib/app/modules/edit_name/profile_bloc.dart
@@ -5,19 +5,21 @@ import 'package:rxdart/rxdart.dart';
 import 'package:techfrenetic/app/common/validators.dart';
 
 class ProfileBloc extends Disposable {
-  final _nameController = BehaviorSubject<String>.seeded("");
-  final _professionController = BehaviorSubject<String>.seeded("");
+  final _nameController = BehaviorSubject<String>();
+  final _professionController = BehaviorSubject<String>();
 
   Stream<String> get nameStream =>
       _nameController.stream.transform(Validators.validateName);
   Stream<String> get professionStream =>
       _professionController.stream.transform(Validators.validateProfession);
+  Stream<bool> get nameFormStream => Rx.combineLatest2(_nameController,
+      _professionController, (nameStream, professionStream) => true);
 
   Function(String) get changeName => _nameController.sink.add;
   Function(String) get changeProfession => _professionController.sink.add;
 
-  String get name => _nameController.value;
-  String get profession => _professionController.value;
+  String? get name => _nameController.valueOrNull;
+  String? get profession => _professionController.valueOrNull;
 
   @override
   void dispose() {
