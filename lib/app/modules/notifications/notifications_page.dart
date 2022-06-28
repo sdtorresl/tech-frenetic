076789383de59f5df5d9ea2d71dd22859103718c
Sergio Torres <sdtorresl@unal.edu.co>
2022-06-28 11:04:05 -0500

    Select notifications

--- lib/app/modules/notifications/notifications_page.dart
+++ lib/app/modules/notifications/notifications_page.dart
@@ -1,6 +1,10 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:techfrenetic/app/models/notification_model.dart';
+import 'package:techfrenetic/app/modules/notifications/notifications_store.dart';
 import 'package:techfrenetic/app/providers/notifications_provider.dart';
 import 'package:techfrenetic/app/widgets/appbar_widget.dart';
 import 'package:timeago/timeago.dart' as timeago;
@@ -13,21 +17,52 @@ class NotificationsPage extends StatefulWidget {
   State<NotificationsPage> createState() => _NotificationsPageState();
 }
 
-class _NotificationsPageState extends State<NotificationsPage> {
+class _NotificationsPageState
+    extends ModularState<NotificationsPage, NotificationsStore> {
   final NotificationsProvider _notificationsProvider = NotificationsProvider();
+
+  @override
+  void initState() {
+    store.clearNotifications();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TFAppBar(
-        title: Text(
-          AppLocalizations.of(context)!.profile_notifications,
-          style: Theme.of(context)
-              .textTheme
-              .headline1!
-              .copyWith(color: Theme.of(context).primaryColor),
-        ),
-      ),
+          title: Text(
+            AppLocalizations.of(context)!.profile_notifications,
+            style: Theme.of(context)
+                .textTheme
+                .headline1!
+                .copyWith(color: Theme.of(context).primaryColor),
+          ),
+          actions: [
+            Observer(
+              builder: (BuildContext context) {
+                return store.selectedNotifications.isNotEmpty
+                    ? Container(
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 10),
+                        child: Badge(
+                          badgeContent: Text(
+                            store.selectedNotifications.length.toString(),
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium
+                                ?.copyWith(color: Colors.white),
+                          ),
+                          child: IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: _deleteSelectedNotifications,
+                          ),
+                        ),
+                      )
+                    : const SizedBox.shrink();
+              },
+            ),
+          ]),
       body: FutureBuilder(
         future: _notificationsProvider.getNotifications(),
         builder: (BuildContext context,
@@ -39,46 +74,53 @@ class _NotificationsPageState extends State<NotificationsPage> {
               itemCount: notifications.length,
               itemBuilder: (context, index) {
                 NotificationModel notification = notifications[index];
-                return Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Material(
-                    borderRadius: BorderRadius.circular(10),
-                    elevation: 2,
-                    color: !notification.read
-                        ? Colors.white
-                        : const Color.fromRGBO(240, 240, 240, 1),
-                    child: ListTile(
-                        onTap: () {
-                          setState(() {
-                            notification.read = !notification.read;
-                          });
-                        },
-                        leading: CircleAvatar(
-                          radius: 20,
-                          backgroundColor: Colors.grey[200],
-                          child: ClipOval(
-                            child: SvgPicture.asset(
-                              "assets/img/avatars/${notification.avatar}.svg",
+                return Observer(
+                  builder: (BuildContext context) {
+                    bool inList =
+                        store.selectedNotifications.contains(notification);
+                    return Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Material(
+                        borderRadius: BorderRadius.circular(10),
+                        elevation: 2,
+                        color: !inList
+                            ? Colors.white
+                            : const Color.fromRGBO(240, 240, 240, 1),
+                        child: ListTile(
+                          onTap: () {
+                            inList
+                                ? store.removeNotification(notification)
+                                : store.addNotification(notification);
+                          },
+                          leading: CircleAvatar(
+                            radius: 20,
+                            backgroundColor: Colors.grey[200],
+                            child: ClipOval(
+                              child: SvgPicture.asset(
+                                "assets/img/avatars/${notification.avatar}.svg",
+                              ),
                             ),
                           ),
+                          contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          title: Text(notification.name),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(notification.body),
+                              Text(
+                                timeago.format(notification.created),
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .caption!
+                                    .copyWith(color: Colors.grey, fontSize: 12),
+                              )
+                            ],
+                          ),
                         ),
-                        contentPadding: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 10),
-                        title: Text(notification.name),
-                        subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(notification.body),
-                            Text(
-                              timeago.format(notification.created),
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .caption!
-                                  .copyWith(color: Colors.grey, fontSize: 12),
-                            )
-                          ],
-                        )),
-                  ),
+                      ),
+                    );
+                  },
                 );
               },
             );
@@ -90,5 +132,13 @@ class _NotificationsPageState extends State<NotificationsPage> {
         },
       ),
     );
+  }
+
+  _deleteSelectedNotifications() {
+    for (NotificationModel notification in store.selectedNotifications) {
+      debugPrint("Delete notification ${notification.body}...");
+    }
+    store.clearNotifications();
+    setState(() {});
   }
 }
---
+++ lib/app/modules/notifications/notifications_store.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
+import 'package:techfrenetic/app/models/notification_model.dart';
+
+part 'notifications_store.g.dart';
+
+class NotificationsStore = _NotificationsStoreBase with _$NotificationsStore;
+
+abstract class _NotificationsStoreBase with Store {
+  @observable
+  ObservableList<NotificationModel> selectedNotifications =
+      ObservableList<NotificationModel>();
+
+  @action
+  clearNotifications() {
+    selectedNotifications.clear();
+  }
+
+  @action
+  addNotification(NotificationModel notification) {
+    selectedNotifications.add(notification);
+  }
+
+  @action
+  removeNotification(NotificationModel notification) {
+    selectedNotifications.remove(notification);
+  }
+}
