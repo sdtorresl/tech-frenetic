ffedd21a9acc48cc96d4f87525585ef016ad4ac1
Sergio Torres <sdtorresl@unal.edu.co>
2022-06-28 16:29:21 -0500

    Change animation

--- lib/app/modules/profile/my_account/my_account_module.dart
+++ lib/app/modules/profile/my_account/my_account_module.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:techfrenetic/app/common/custom_transitions.dart';
 
 import './my_account_controller.dart';
 import 'my_account_page.dart';
@@ -11,6 +12,11 @@ class MyAccountModule extends Module {
 
   @override
   final List<ModularRoute> routes = [
-    ChildRoute(Modular.initialRoute, child: (_, args) => const MyAccountPage()),
+    ChildRoute(
+      Modular.initialRoute,
+      child: (_, args) => const MyAccountPage(),
+      transition: TransitionType.custom,
+      customTransition: scaleAndFadeTransition,
+    ),
   ];
 }
--- lib/app/modules/profile/profile_module.dart
+++ lib/app/modules/profile/profile_module.dart
@@ -1,7 +1,7 @@
+import 'package:techfrenetic/app/common/custom_transitions.dart';
 import 'package:techfrenetic/app/modules/profile/my_profile/my_profile_page.dart';
 import 'my_account/my_account_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:techfrenetic/app/common/custom_transitions.dart';
 import 'package:techfrenetic/app/modules/profile/my_activity/my_activity_page.dart';
 import 'package:techfrenetic/app/modules/profile/my_content/my_content_page.dart';
 import 'package:techfrenetic/app/modules/profile/profile_page.dart';
@@ -19,19 +19,32 @@ class ProfileModule extends Module {
     ChildRoute(
       Modular.initialRoute,
       child: (_, args) => const ProfilePage(),
-      transition: TransitionType.custom,
-      customTransition: scaleAndFadeTransition,
       children: [
         ChildRoute(
           '/profile',
           child: (context, args) =>
               MyProfilePage(userId: args.params['userId']),
+          transition: TransitionType.custom,
+          customTransition: scaleAndFadeTransition,
         ),
-        ChildRoute('/content', child: (context, args) => const MyContentPage()),
-        ChildRoute('/activity',
-            child: (context, args) => const MyActivityPage()),
-        ChildRoute('/saved-articles',
-            child: (context, args) => const MySavedArticlesPage()),
+        ChildRoute(
+          '/content',
+          child: (context, args) => const MyContentPage(),
+          transition: TransitionType.custom,
+          customTransition: scaleAndFadeTransition,
+        ),
+        ChildRoute(
+          '/activity',
+          child: (context, args) => const MyActivityPage(),
+          transition: TransitionType.custom,
+          customTransition: scaleAndFadeTransition,
+        ),
+        ChildRoute(
+          '/saved-articles',
+          child: (context, args) => const MySavedArticlesPage(),
+          transition: TransitionType.custom,
+          customTransition: scaleAndFadeTransition,
+        ),
         ModuleRoute(
           '/my-account',
           module: MyAccountModule(),
