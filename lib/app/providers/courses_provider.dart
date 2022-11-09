9ecd22ea5d98b64edbee4bd7a4c16c459434e0a6
Sergio Torres <sdtorresl@unal.edu.co>
2022-11-09 11:25:51 -0500

    Added Paginator

--- lib/app/providers/courses_provider.dart
+++ lib/app/providers/courses_provider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:techfrenetic/app/models/dto/paginator.dart';
 import 'package:techfrenetic/app/models/courses_model.dart';
+import 'package:techfrenetic/app/models/lesson_model.dart';
 import 'package:techfrenetic/app/providers/tf_provider.dart';
 
 import 'package:http/http.dart' as http;
@@ -30,16 +32,12 @@ class CoursesProvider extends TechFreneticProvider {
     CourseModel? course;
 
     try {
-      Uri _url = Uri.parse("$baseUrl/api/$locale/v1/courses");
+      Uri _url = Uri.parse("$baseUrl/api/$locale/v1/internal-course/$id");
       var response = await http.get(_url);
 
       if (response.statusCode == 200) {
-        Map<String, dynamic> jsonResponse = json.jsonDecode(response.body);
-        List<CourseModel> courses = List<CourseModel>.from(
-            jsonResponse["articles"].map((x) => CourseModel.fromJson(x)));
-        // TODO: Update method
-
-        return courses.first;
+        List<dynamic> jsonResponse = json.jsonDecode(response.body);
+        course = CourseModel.fromJson(jsonResponse.first);
       } else {
         debugPrint('Request failed with status: ${response.statusCode}.');
       }
@@ -47,5 +45,27 @@ class CoursesProvider extends TechFreneticProvider {
       debugPrint(e.toString());
     }
     return course;
+  }
+
+  Future<Paginator<LessonModel>> getVideosByCourse(int id,
+      {int page = 0}) async {
+    Paginator<LessonModel> paginator = Paginator(
+        currentPage: page, itemsPerPage: 0, totalItems: 0, totalPages: 0);
+
+    try {
+      Uri _url = Uri.parse(
+          "$baseUrl/api/$locale/v1/internal-course-videos/$id?page=$page");
+      var response = await http.get(_url);
+
+      if (response.statusCode == 200) {
+        Map<String, dynamic> jsonResponse = json.jsonDecode(response.body);
+        paginator = Paginator.fromMap(jsonResponse, LessonModel.fromMap);
+      } else {
+        debugPrint('Request failed with status: ${response.statusCode}.');
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+    return paginator;
   }
 }
